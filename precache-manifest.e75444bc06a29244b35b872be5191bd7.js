7c607dc4fcb4c0f9dfebb8d950241e50fd004eb6
unknown <dinkun98@gmail.com>
2022-09-20 15:45:37 +0700

    Updates

---
+++ precache-manifest.e75444bc06a29244b35b872be5191bd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afeb69ecf3b143c5848f6430de0d2b28",
+    "url": "/booking-tickets/index.html"
+  },
+  {
+    "revision": "8998d8296d8483c75104",
+    "url": "/booking-tickets/static/css/main.64c400c3.chunk.css"
+  },
+  {
+    "revision": "e216a9de82f87173f595",
+    "url": "/booking-tickets/static/js/2.44691ff4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/booking-tickets/static/js/2.44691ff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8998d8296d8483c75104",
+    "url": "/booking-tickets/static/js/main.0dd1953b.chunk.js"
+  },
+  {
+    "revision": "3e5ae58c4e12e92659ab",
+    "url": "/booking-tickets/static/js/runtime-main.b2d8b6e2.js"
+  },
+  {
+    "revision": "1716f3f87bf9af14a763249e77f202da",
+    "url": "/booking-tickets/static/media/Pony.1716f3f8.ttf"
+  }
+]);
